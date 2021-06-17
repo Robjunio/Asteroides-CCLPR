bcadb1e969281270b70da6b50b28b9b7f1f256d0
Roberto Gomes <rjrg.snf20@uea.edu.br>
2021-06-16 23:43:53 -0300

    Inserting the GameController class

---
+++ GameController.py
@@ -0,0 +1,17 @@
+from util import *
+from random import randint
+
+
+class GameController():
+    def __init__(self):
+        nave_bot = []
+        asteroides = []
+        score = 0
+
+    def random_spaw(self):
+        pass
+
+
+    def horda(self, num_horda):
+        pass
+
