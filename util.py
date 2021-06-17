485ad54b3f8b03d0c3aae30f81f0aa16d272e385
Roberto Gomes <rjrg.snf20@uea.edu.br>
2021-06-16 23:44:18 -0300

    Arquivo de utilidades

---
+++ util.py
@@ -0,0 +1,24 @@
+import sys
+import pygame
+
+
+def collision(pos1, pos2):
+    pass
+
+
+def quit():
+    pygame.quit()
+    exit()
+
+    
+# Color's
+WHITE = (255, 255, 255)
+BLACK  = (0, 0, 0)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+
+# Initial Position of the ship
+starting_pos = (0, 0)
+
+# Reset Values
+new_score = 0
