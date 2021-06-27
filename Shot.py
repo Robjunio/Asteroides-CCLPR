0da8290d0c080fdfd7aec144f6763b3caf5537c2
CoimbriceUEA <lhce.snf20@uea.edu.br>
2021-06-27 18:51:45 -0400

    shot update

--- Shot.py
+++ Shot.py
@@ -7,4 +7,5 @@ class Shot():
     def __init__(self, bullet_speed, bullet_pos):
         self.bullet_speed = bullet_speed
         self.bullet_pos = bullet_pos
-        
+        self.sprite = pygame.image.load("assets/bullet.png")
+        self.radius = self.sprite.get_width() / 2
