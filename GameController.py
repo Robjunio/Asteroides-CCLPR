f42b056baf3729c016419be7ec6e14646c66c14f
Roberto <rjrg.snf20@uea.edu.br>
2021-06-28 00:02:16 -0300

    #add: random_spawn, all_dead

--- GameController.py
+++ GameController.py
@@ -1,17 +1,34 @@
-from util import *
+import util
+from SpaceShip import SpaceShip
 from random import randint
 
 
 class GameController():
     def __init__(self):
-        nave_bot = []
+        nave_bots = []
         asteroides = []
         score = 0
+        wave = 1
 
-    def random_spaw(self):
+    def random_spawn(self):
+        pos_x, pos_y = randint(0, util.WINDOW_wIDTH), randint(0, util.WINDOW_HEIGTH)
+        if SpaceShip.position[0] == pos_x or SpaceShip.position[1] == pos_y:
+            return random_spawn()
+        else:
+            return pos_x, pos_y
+
+
+    def start_wave(self, num_horda):
         pass
 
 
-    def horda(self, num_horda):
-        pass
+    def all_dead(self):
+        if len(nave_bots) == 0 and len(asteroides) == 0:
+            wave += 1
+            start_wave(wave)
+        else:
+            continue
+
+    def create_enemy(self)
+        
 
